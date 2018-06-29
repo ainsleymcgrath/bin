40eb8c7cbb1c4a39500988244dc976c4de50fa52
Ainsley McGrath <ainsley.mgrath@aptitive.com>
2018-06-29 09:24:50 -0500

    breaking changes... add in prompts to create a new pipenv

--- .pydev.sh
+++ .pydev.sh
@@ -1,38 +1,105 @@
 #!/bin/bash
+set -e
 
-# a function to ease python development
+RED='\033[0;31m'
+BLUE='\e[34mBlue'
+GRAY='\e[90mDark'
+NO_COLOR='\033[0m'
+CLEAR_LINE='\r\033[K'
+
+# to ease python boilerplate
 function pystart() {
 
     #gracious welcome
-    echo "Hey, dude. Welcome back.\n"
+    printf "${CLEAR_LINE}👋 ${BLUE} Hey, dude. Welcome back.\n"
 
     # got pipenv?
     if [ -f "Pipfile" ] ;then
-        echo "Activating pipenv..."
+
+        printf "${CLEAR_LINE}🐍 ${NO_COLOR} Activating pipenv...\n"
         pipenv shell
-        echo "Done!"
+        printf "${CLEAR_LINE}✔️ ${GRAY} Done!\n"
 
         # developing a flask app?
         if grep "flask" Pipfile --quiet ;then
-            echo "Debug mode?"
+
+            printf "${CLEAR_LINE}🐞 ${NO_COLOR} Debug mode?\n"
+            
             read answer
             if [[ "$answer" =~  "[yY]" ]] ;then
-                echo "Setting up flask debugging..."
-                export FLASK_DEBUG=1      
+
+                printf "Setting up flask debugging...\n"
+                export FLASK_DEBUG=1   
+                printf "${CLEAR_LINE}✔️ ${GRAY} Done!\n" 
+
             else 
-                echo "No debug for you."
+
+                printf "${CLEAR_LINE} ${BLUE} No debug for you. 🙅‍\n"
                 export FLASK_DEBUG=0
+
             fi          
+
+            # TODO: prompt to pick a .py file as FLASK_APP
+
         else
-           echo "You're env is ready, my dear."
+
+           printf "${CLEAR_LINE} ${BLUE}You're env is ready, my dear. 😊\n"
+
         fi
+
     else 
-        echo "Would you like to create a new pipenv?"
+
+        printf "${CLEAR_LINE} ${BLUE} Would you like to create a new pipenv?\n"
+
         read answer
         if [[ "$answer" =~  "[yY]" ]] ;then
-            echo "Let's do it!"
+
+            printf "Let's do it!\n 🥈 or 🥉?\n"
+
+            read answer
+            if [[ answer =~ "three|3" ]] ;then
+            
+                printf "${CLEAR_LINE}🐍 ${NO_COLOR} Creating python3 pipenv...\n"
+                pipenv --three
+                printf "${CLEAR_LINE}✔️ ${GRAY} Done!\n" 
+
+                printf "${CLEAR_LINE}🐍 ${NO_COLOR} Activating pipenv...\n"
+                pipenv shell
+                printf "${CLEAR_LINE}✔️ ${GRAY} Done!\n"
+
+            else 
+            
+                printf "${CLEAR_LINE}🐍 ${NO_COLOR} Creating python2 pipenv...\n"
+                pipenv --two
+                printf "${CLEAR_LINE}✔️ ${GRAY} Done!\n" 
+
+                printf "${CLEAR_LINE}🐍 ${NO_COLOR} Activating pipenv...\n"
+                pipenv shell
+                printf "${CLEAR_LINE}✔️ ${GRAY} Done!\n"
+
+            fi
+
         else
-            echo "Alright then...."
+
+            printf "${CLEAR_LINE} ${BLUE} There's nothing for us to do then bye. 🙆‍♂️\n"
+        
         fi
+    
     fi
+}
+
+
+# to destroy the env and un-python the directory
+function pystop() {
+    
+    printf "${CLEAR_LINE}🐍 ${NO_COLOR} Dectivating pipenv...\n"
+    deactivate #TODO: check if this works``
+    printf "${CLEAR_LINE}✔️ ${GRAY} Done!\n"
+    
+}
+
+
+# to destroy the env and un-python the directory
+function pysmash() {
+
 }
